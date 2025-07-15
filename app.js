021edc607ea5120d95505e84a81d20f59e1266a0
shrujal <shrujaldoshi907@gmail.com>
2025-07-15 22:06:32 +0530

    add further

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejs_mate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError.js");
+const { error } = require("console");
+const {listingSchema} = require("./schema.js");
+
+
+app.engine('ejs',ejs_mate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({ extended: true }));
 
 main()
@@ -22,13 +30,23 @@ async function main() {
 
 app.get("/",(req,res) => {
     res.send("server is working");
-})
+});
+
+const validateListing = (req,res,next) => {
+  const {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400,errMsg);
+  }
+  else
+    next();
+}
 
 // Index Route
-app.get("/listings",async (req,res) => {
+app.get("/listings",wrapAsync (async (req,res) => {
    const allListings = await Listing.find({});
    res.render("listings/index.ejs",{allListings});
-});
+}));
 
 //New Route
 app.get("/listings/new",(req,res) => {
@@ -36,40 +54,53 @@ app.get("/listings/new",(req,res) => {
 });
 
 // Show Route
-app.get("/listings/:id",async (req,res) => {
-   let {id} = req.params;
-   const listing = await Listing.findById(id)
-   res.render("listings/show.ejs",{listing});
-});
+app.get("/listings/:id", 
+  wrapAsync (async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    console.log("Listing details:", listing);
+    res.render("listings/show.ejs",{listing});
+  })
+);
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post("/listings",
+  validateListing,
+  wrapAsync (async (req, res , next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
+app.get("/listings/:id/edit", 
+  wrapAsync (async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put("/listings/:id", 
+  validateListing,
+  wrapAsync (async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-});
+app.delete("/listings/:id", 
+  wrapAsync (async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
 
 // app.get("/testListing", async (req,res) => {
 //     let sampleListing = new Listing(
@@ -86,6 +117,18 @@ app.delete("/listings/:id", async (req, res) => {
 //     console.log("sample was saved");
 //     res.send("successful testing");
 // });
+
+// app.all("*",(req,res,next) => {
+//   next(new ExpressError(404,"Page Not Found!"));
+// });
+
+// Middleware for Create Route
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs",{err});
+  // res.status(statusCode).send(message);
+});
+
 
 app.listen(8080,() => {
     console.log("server is listening to port 8080");
